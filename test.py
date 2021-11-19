228aecc6a23ac8155b569fc2b231ab494d70fd0e
emix1984 <2909755020@qq.com>
2021-11-19 23:40:51 +0900

    glowpick获取网页review数据

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+import parsel
+
+
+1 找数据对应的链接地址
+url = 'https://www.ababbb.com/ymm'
+
+#伪装，代表浏览器身份，python的基本数据容器（列表，元组，字典，集合）
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.214 Safari/537.36'}
+
+#2 发送指定地址请求（python）utf-8
+response = requests.get(url=url, headers=headers)
+
+html_data = response.text
+
+print(html_data)
