7b1bc81c69ce618cd0700009600c83b40f3381fe
emix1984 <2909755020@qq.com>
2021-12-06 23:24:00 +0900

    采集oliveyoung美妆品类产品3步代码完毕 从selenium改成request post方式，调试detailpage采集后需要改善tab属性采集的方式逻辑，中间涉及到电子用品和医用仪器的属性，进行了属性采集优化。

--- oliveyoung韩语 20211119/version01 oliveyoung category 单线程/02 oliveyoung listing 20211119.py
+++ oliveyoung韩语 20211119/version01 oliveyoung category 单线程/02 oliveyoung listing 20211119.py
@@ -19,6 +19,7 @@
 
 from time import sleep
 import requests
+import parsel
 import csv
 import re
 import time
@@ -42,28 +43,50 @@ time_1 = time.time()
 # 时间戳
 rightnow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-# 提取三级分类地址链接和页面数量
-with open('01_oliveyoung_category.csv', mode='r', encoding='utf-8') as f:
-    reader = csv.reader(f)
-    fieldnames = next(reader)
-    # print(fieldnames)
-    csv_reader = csv.DictReader(f, fieldnames=fieldnames)  # self._fieldnames = fieldnames
-    # list of keys for the dict 以list的形式存放键名
-    for row in csv_reader:
-        d = {}
-        for k, v in row.items():
-            d[k] = v
-        category1 = list(d.values())[0]
-        category2 = list(d.values())[2]
-        category3 = list(d.values())[4]
-        category3_code = list(d.values())[5]
-        # url_category3 = list(d.values())[6]  # 提取三级分类网址链接生成列表
-        pageno_raw = list(d.values())[8] # 选取三级分类网址下总页面数
-        pageno = int(pageno_raw) # 将三级分类网址下总页面数从文字类型转换为数值型
-        print(category1, category2, category3, pageno)
+# 配置oliveyoung主页地址，浏览器基础信息
+url = f'https://www.oliveyoung.co.kr/store/company/brandStory.do'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.99 Safari/537.36',
+    'Connection': 'close'
+}
 
-        for page in range(1, pageno+1): # 因为range函数计算的是区间，开始的数字是1，所以计算出来的页码+1
-            print(f'==================正在爬取',category1,category2, category3, f'第{page}页内容=======================')
+# 发送请求
+response = requests.get(url=url, headers=headers)
+html_data = response.text
+
+# 解析html代码，获取主要分类名称和编码
+selector = parsel.Selector(html_data)
+category2_names = selector.xpath('//*[@id="gnbAllMenu"]/ul/li[1]/div/ul/li/a/text()').getall()
+category2_codes = selector.xpath('//*[@id="gnbAllMenu"]/ul/li[1]/div/ul/li/a[@data-ref-dispcatno]/@data-ref-dispcatno').getall()
+
+for category2_name, category2_code in zip(category2_names,category2_codes):
+    url_category_2 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category2_code}&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&gateCd=Drawer&trackingCd=Cat{category2_code}_MID'
+
+    # 发送请求
+    response_category_2 = requests.get(url=url_category_2, headers=headers)
+    html_data_category_2 = response_category_2.text
+    # print(html_data_category_2)
+
+    # 解析html代码，获取主要分类名称和编码
+    selector_html_data_category_2 = parsel.Selector(html_data_category_2)
+    category3_names = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a/text()').getall()
+    category3_codes = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a[@class]/@class').getall()
+
+    for category3_name, category3_code in zip(category3_names, category3_codes):
+        url_category_3 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category3_code}&fltDispCatNo=&prdSort=01&pageIdx=1&rowsPerPage=24&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=0&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat{category3_code}_Small'
+
+        # 发送请求
+        response_category_3 = requests.get(url=url_category_3, headers=headers)
+        html_data_category_3 = response_category_3.text
+        # print(html_data_category_3)
+        selector_html_data_category_3 = parsel.Selector(html_data_category_3)
+        number_raw = selector_html_data_category_3.xpath('//*[@id="Contents"]/p/span/text()').getall()[0]
+        number = number_raw.strip()
+        page_number = int(int(number) / 48) + 1
+        # print(category2_name, category3_name, category3_code, number, page_number)
+
+        for page in range(1, page_number+1): # 因为range函数计算的是区间，开始的数字是1，所以计算出来的页码+1
+            print(f'==================正在爬取', category2_name, category3_name, f'第{page}页内容=======================')
             prdSort = "3" # 产品列表按照01人气排序；02最新登录排序；03按照销量排序；04按照最低价格排序；05按照最高价格排序
             rowsPerPage = "48" # 每页显示产品数 24, 36, 48
             url = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category3_code}&fltDispCatNo=&prdSort={prdSort}&pageIdx={page}&rowsPerPage={rowsPerPage}&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat{category3_code}_Small'
@@ -73,7 +96,7 @@ with open('01_oliveyoung_category.csv', mode='r', encoding='utf-8') as f:
             sleep(1)  # 等待页面加载0.5秒
             response = requests.get(url=url, headers=headers)
             print(response)
-            sleep(6)  # 等待页面加载3秒
+            sleep(3)  # 等待页面加载3秒
             html_data = response.text
             # print(html_data)
 
@@ -89,18 +112,18 @@ with open('01_oliveyoung_category.csv', mode='r', encoding='utf-8') as f:
 
             for d_url in urls:
                 prd_name_number = re.findall(r'goodsNo=(.*?)&', d_url)[0]
-                print(category, pageno, number, prd_name_number)
+                print(category, page_number, number, prd_name_number)
 
                 # 建立字典存储到文件
                 dict = {
-                    '三级类目名称': category3,
+                    '三级类目名称': category3_name,
                     '三级类目编码': category3_code,
                     '三级类目产品数': number,
-                    '页码': page,
+                    '页码': page_number,
                     '产品编码': prd_name_number,
                     '产品链接': d_url,
                     '采集时间': rightnow,
                 }
-                print(rightnow, '....正在采集：', category1, category2, category3, category)
+                print(rightnow, '....正在采集：', category2_name, category3_name,page)
                 csv_writer.writerow(dict)
         print(f'===采集完成===累计耗时：', time.time() - time_1, rightnow)
--- oliveyoung韩语 20211119/version01 oliveyoung category 单线程/test01.py
+++ oliveyoung韩语 20211119/version01 oliveyoung category 单线程/test01.py
@@ -1,9 +1,57 @@
-import random
+# import random
+# import re
+# bi_5 = '■ 제조자 : Otsuka Phamrceuticl Co., Ltd  ■ 판매자 : 한국오츠카제약'
+# bi_5_1_raw = re.findall(r"제조.*:(.*)■", bi_5, re.S)[0]
+# bi_5_1 = "".join(bi_5_1_raw).strip()
+# bi_5_2_raw = re.findall(r"판매.*:(.*)", bi_5, re.S)[0]
+# bi_5_2 = "".join(bi_5_2_raw).strip()
+# print('=1======', bi_5_1)
+# print('=2======', bi_5_2)
+
+import requests
+import parsel
+import csv
 import re
-bi_5 = '■ 제조자 : Otsuka Phamrceuticl Co., Ltd  ■ 판매자 : 한국오츠카제약'
-bi_5_1_raw = re.findall(r"제조.*:(.*)■", bi_5, re.S)[0]
-bi_5_1 = "".join(bi_5_1_raw).strip()
-bi_5_2_raw = re.findall(r"판매.*:(.*)", bi_5, re.S)[0]
-bi_5_2 = "".join(bi_5_2_raw).strip()
-print('=1======', bi_5_1)
-print('=2======', bi_5_2)
+import time
+
+# 配置oliveyoung主页地址，浏览器基础信息
+url = f'https://www.oliveyoung.co.kr/store/company/brandStory.do'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.99 Safari/537.36',
+    'Connection': 'close'
+}
+
+# 发送请求
+response = requests.get(url=url, headers=headers)
+html_data = response.text
+
+# 解析html代码，获取主要分类名称和编码
+selector = parsel.Selector(html_data)
+category2_names = selector.xpath('//*[@id="gnbAllMenu"]/ul/li[1]/div/ul/li/a/text()').getall()
+category2_codes = selector.xpath('//*[@id="gnbAllMenu"]/ul/li[1]/div/ul/li/a[@data-ref-dispcatno]/@data-ref-dispcatno').getall()
+
+for category2_name,category2_code in zip(category2_names,category2_codes):
+    url_category_2 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category2_code}&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&gateCd=Drawer&trackingCd=Cat{category2_code}_MID'
+
+    # 发送请求
+    response_category_2 = requests.get(url=url_category_2, headers=headers)
+    html_data_category_2 = response_category_2.text
+    # print(html_data_category_2)
+
+    # 解析html代码，获取主要分类名称和编码
+    selector_html_data_category_2 = parsel.Selector(html_data_category_2)
+    category3_names = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a/text()').getall()
+    category3_codes = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a[@class]/@class').getall()
+
+    for category3_name, category3_code in zip(category3_names, category3_codes):
+        url_category_3 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category3_code}&fltDispCatNo=&prdSort=01&pageIdx=1&rowsPerPage=24&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=0&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat{category3_code}_Small'
+
+        # 发送请求
+        response_category_3 = requests.get(url=url_category_3, headers=headers)
+        html_data_category_3 = response_category_3.text
+        # print(html_data_category_3)
+        selector_html_data_category_3 = parsel.Selector(html_data_category_3)
+        number_raw = selector_html_data_category_3.xpath('//*[@id="Contents"]/p/span/text()').getall()[0]
+        number = number_raw.strip()
+        page_number = int(int(number) / 48) + 1
+        print(category2_name, category3_name,category3_code, number,page_number)
