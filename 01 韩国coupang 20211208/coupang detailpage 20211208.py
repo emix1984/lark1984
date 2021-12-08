427392e28f9ce9a7cd3ae09e57fc4da343c9fae3
2909755020@qq.com <Liuying!o28>
2021-12-09 01:00:40 +0900

    尝试采集多线程oliveyoung产品列表

---
+++ 01 韩国coupang 20211208/coupang detailpage 20211208.py
@@ -0,0 +1,55 @@
+from time import sleep
+import requests
+import parsel
+import csv
+import re
+import time
+import concurrent.futures
+
+def get_response(html_url):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44',
+    }
+    response = requests.get(html_url, headers=headers)
+    html_data_url = response.text
+    return html_data_url
+
+def get_selector(html_url):
+    html_data_url = get_response(html_url)
+    selector = parsel.Selector(html_data_url)
+    return selector
+
+url_detailpage = f'https://www.coupang.com/vp/products/344529480?itemId=148293960&vendorItemId=3335127002&pickType=COU_PICK&q=%EB%A1%9C%EC%85%98&itemsCount=36&searchId=b5738182b86f48b9a249acf9e1f240a3&rank=1&isAddedCart='
+selector_detailpage = get_selector(url_detailpage)
+
+# 产品名
+prod_buy_header_title = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[3]/h2/text()').get()
+# 产品评论数
+reviews_count = selector_detailpage.xpath('//*[@id="prod-review-nav-link"]/span[2]/text()').get()
+# 折扣sale
+discount_rate = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[1]/text()').get().strip()
+# 市场价
+origin_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[2]/text()').get()
+# 销售价
+prod_sale_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[1]/strong/text()').get()
+# 每毫升克容量单价
+unit_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[2]/text()').get()
+## prod-description
+# prod-description-attribute
+prod_attr_item = selector_detailpage.xpath('//*[@class="prod-description"]/ul/li/text()').get()
+# print(prod_attr_item)
+print(prod_buy_header_title,reviews_count,discount_rate,origin_price,prod_sale_price,unit_price,)
+
+
+## tab-titles
+# product-item__table
+prod_attr_info = selector_detailpage.xpath('//*[@id="itemBrief"]/div/table/tbody/tr[1]/td[2]').get()
+print('product-item__table')
+print(prod_attr_info)
+
+## 产品销售者信息 product-item__table product-seller
+product_seller = selector_detailpage.xpath('/html/body/div[2]/section/div[2]/div[10]/ul[2]/li[4]/div/table/tbody/tr/td').extract()
+print('产品销售者信息 product-item__table product-seller')
+print(product_seller)
+
+##
