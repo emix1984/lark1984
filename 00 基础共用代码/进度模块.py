90ef5cd011399517e92081ba1f38eb67eda08035
2909755020@qq.com <Liuying!o28>
2021-12-12 20:12:01 +0900

    尝试采集多线程oliveyoung产品列表

---
+++ 00 基础共用代码/进度模块.py
@@ -0,0 +1,21 @@
+#
+# https://zhuanlan.zhihu.com/p/360444190
+
+
+# # tqdm模块
+#
+# import time
+# from tqdm import tqdm
+# for i in tqdm(range(100)):
+#     time.sleep(0.05)
+
+
+# # progress模块
+#
+# import time
+# from progress.bar import Bar
+# #可以通过fill设置进度条填充符号，默认“#”
+# #可以通过suffix设置成百分比显示
+# bar = Bar("Loading", fill='$', max = 100, suffix = '%(percent)d%%')
+# for i in bar.iter(range(100)):
+#     time.sleep(0.01)
