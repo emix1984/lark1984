a5c51a73ab4d9ab6c80d1ff15999bc42e6247996
emix1984 <2909755020@qq.com>
2021-12-13 19:24:26 +0900

    修改文件夹归类形式

--- 01韩国 电商 oliveyoung韩语 20211119/version02 oliveyoung category 多线程/02 oliveyoung listing 20211207.py
+++ 01韩国 电商 oliveyoung韩语 20211119/version02 oliveyoung category 多线程/02 oliveyoung listing 20211207.py
@@ -60,30 +60,25 @@ def open_csv_data():
 
 
 def parse_data(category2_names, category2_codes):
-for category2_name, category2_code in zip(category2_names,category2_codes):
-    url_category_2 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category2_code}&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&gateCd=Drawer&trackingCd=Cat{category2_code}_MID'
-
-    # 发送请求
-    selector_html_data_category_2 = get_selector(url_category_2)
-
-    category3_names = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a/text()').getall()
-    category3_codes = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a[@class]/@class').getall()
-
-    for category3_name, category3_code in zip(category3_names, category3_codes):
-        url_category_3 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category3_code}&fltDispCatNo=&prdSort=01&pageIdx=1&rowsPerPage=24&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=0&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat{category3_code}_Small'
+    for category2_name, category2_code in zip(category2_names,category2_codes):
+        url_category_2 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category2_code}&isLoginCnt=1&aShowCnt=0&bShowCnt=0&cShowCnt=0&gateCd=Drawer&trackingCd=Cat{category2_code}_MID'
 
         # 发送请求
-        selector_html_data_category_3 = get_selector(url_category_3)
-        # print(html_data_category_3)
-        number_raw = selector_html_data_category_3.xpath('//*[@id="Contents"]/p/span/text()').getall()[0]
-        number = number_raw.strip()
-        page_number = int(int(number) / 48) + 1
-        # print(category2_name, category3_name, category3_code, number, page_number)
+        selector_html_data_category_2 = get_selector(url_category_2)
 
-        if __name__ == '__main__':
-            # concurrent.futures模块提供了两种executor的子类，ThreadPoolExecutor各自独立操作一个线程和 ProcessPoolExecutor一个进程池
-            exe = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-            # exe = concurrent.futures.ProcessPoolExecutor(max_workers=10)
+        category3_names = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a/text()').getall()
+        category3_codes = selector_html_data_category_2.xpath('//*[@id="Contents"]/ul[1]/li/a[@class]/@class').getall()
+
+        for category3_name, category3_code in zip(category3_names, category3_codes):
+            url_category_3 = f'https://www.oliveyoung.co.kr/store/display/getMCategoryList.do?dispCatNo={category3_code}&fltDispCatNo=&prdSort=01&pageIdx=1&rowsPerPage=24&searchTypeSort=btn_thumb&plusButtonFlag=N&isLoginCnt=0&aShowCnt=0&bShowCnt=0&cShowCnt=0&trackingCd=Cat{category3_code}_Small'
+
+            # 发送请求
+            selector_html_data_category_3 = get_selector(url_category_3)
+            # print(html_data_category_3)
+            number_raw = selector_html_data_category_3.xpath('//*[@id="Contents"]/p/span/text()').getall()[0]
+            number = number_raw.strip()
+            page_number = int(int(number) / 48) + 1
+            # print(category2_name, category3_name, category3_code, number, page_number)
 
             for page in range(1, page_number+1): # 因为range函数计算的是区间，开始的数字是1，所以计算出来的页码+1
                 print(f'==================正在爬取', category2_name, category3_name, f'第{page}页内容=======================')
@@ -120,8 +115,7 @@ for category2_name, category2_code in zip(category2_names,category2_codes):
                     }
                     print(rightnow, '....正在采集：', category2_name, category3_name, page, "/", page_number)
                     csv_writer.writerow(dict)
-        exe.shutdown()
-        print(f'===采集完成===累计耗时：', time.time() - time_1, rightnow)
+            print(f'===采集完成===累计耗时：', time.time() - time_1, rightnow)
 
 #
 def run():
@@ -140,13 +134,13 @@ def run():
 # #     run(f'https://www.fabiaoqing.com/biaoqing/lists/page/{page}.html')
 # # print(f'总耗时，{time.time() - time_1}')
 #
-if __name__ == '__main__':
-    time_1 = time.time()
-    # concurrent.futures模块提供了两种executor的子类，ThreadPoolExecutor各自独立操作一个线程和 ProcessPoolExecutor一个进程池
-    exe = concurrent.futures.ThreadPoolExecutor(max_workers=10)
-    # exe = concurrent.futures.ProcessPoolExecutor(max_workers=10)
-    for page in range(1, 11):
-        url = f'https://www.fabiaoqing.com/biaoqing/lists/page/{page}.html'
-        exe.submit(run, url)
-    exe.shutdown()
-    print(f'总耗时:{time.time()-time_1}')
+# if __name__ == '__main__':
+#     time_1 = time.time()
+#     # concurrent.futures模块提供了两种executor的子类，ThreadPoolExecutor各自独立操作一个线程和 ProcessPoolExecutor一个进程池
+#     exe = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+#     # exe = concurrent.futures.ProcessPoolExecutor(max_workers=10)
+#     for page in range(1, 11):
+#         url = f'https://www.fabiaoqing.com/biaoqing/lists/page/{page}.html'
+#         exe.submit(run, url)
+#     exe.shutdown()
+#     print(f'总耗时:{time.time()-time_1}')
