b439b645366a45edc5f955c22410d8b6168440ac
2909755020@qq.com <Liuying!o28>
2021-12-22 20:21:45 +0900

    coupang从主分类到第五级分类清单整理完毕，采集产品列表页面测试完毕 20211220

--- 01韩国 电商 Coupang 20211208/03_coupang_detailpage 20211208.py
+++ 01韩国 电商 Coupang 20211208/03_coupang_detailpage 20211208.py
@@ -70,6 +70,7 @@ for url_detailpage in urls_detailpage:
     # 计数
     n = n+1
     print(f'>>>正在采集第\033[32m{n}\033[0m条数据', url_detailpage)
+    url_detailpage = 'https://www.coupang.com/vp/products/246379487?itemId=780629226&vendorItemId=79769806875&sourceType=CATEGORY&categoryId=486148'
 
     productId = re.findall(f'products/(.*?)\?', url_detailpage)[0]
     itemId = re.findall(f'itemId=(.*?)&', url_detailpage)[0]
@@ -80,173 +81,209 @@ for url_detailpage in urls_detailpage:
         }
     response = requests.get(url=url_detailpage, headers=headers)
     html_data_url = response.text
-    selector_detailpage = parsel.Selector(html_data_url)
+    if "상품을 찾을 수 없습니다"not in html_data_url:
 
-    # 类目
-    category1_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[2]/a[@title]/@title').get()
-    category2_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[3]/a[@title]/@title').get()
-    category3_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[4]/a[@title]/@title').get()
+        selector_detailpage = parsel.Selector(html_data_url)
 
-    # brand name
-    brand_name = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/a/text()').get()
-    # 品牌链接
-    brand_url = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/a[@href]/@href').get()
+        # 类目
+        category1_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[2]/a[@title]/@title').get()
+        category2_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[3]/a[@title]/@title').get()
+        category3_name = selector_detailpage.xpath('//*[@id="breadcrumb"]/li[4]/a[@title]/@title').get()
 
-    # 产品名
-    prod_buy_header_title = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[3]/h2/text()').get()
-    # 产品缩略图
-    dp_img_raw = selector_detailpage.xpath('//*[@id="repImageContainer"]/img[@src]/@src').get()
-    dp_img = "https:"+dp_img_raw
+        # brand name
+        brand_name = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/a/text()').get()
+        # 品牌链接
+        brand_url = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/a[@href]/@href').get()
 
-    # 产品评论数
-    reviews_count = selector_detailpage.xpath('//*[@id="prod-review-nav-link"]/span[2]/text()').get()
-    # 折扣sale
-    discount_rate = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[1]/text()').get().strip()
-    # 市场价
-    origin_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[2]/text()').get()
-    # 销售价
-    prod_sale_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[1]/strong/text()').get()
-    # 每毫升克容量单价
-    unit_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[2]/text()').get()
+        # 产品名
+        prod_buy_header_title = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[3]/h2/text()').get()
+        # 产品缩略图
+        dp_img_raw = selector_detailpage.xpath('//*[@id="repImageContainer"]/img[@src]/@src').get()
+        dp_img = "https:"+dp_img_raw
 
-    ## 产品主要卖点prod-description
-    # prod-description-attribute
-    # prod_attr_item = selector_detailpage.xpath('//*[@class="prod-description"]/ul/li/text()').get()
-    # print(prod_attr_item)
-    # print(prod_buy_header_title, reviews_count,discount_rate,origin_price,prod_sale_price,unit_price,)
+        # 产品评论数
+        reviews_count = selector_detailpage.xpath('//*[@id="prod-review-nav-link"]/span[2]/text()').get()
+        # 折扣sale
+        discount_rate = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[1]/text()').get().strip()
+        # 市场价
+        origin_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[1]/span[2]/text()').get()
+        # 销售价
+        prod_sale_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[1]/strong/text()').get()
+        # 每毫升克容量单价
+        unit_price = selector_detailpage.xpath('//*[@id="contents"]/div[1]/div/div[3]/div[5]/div[1]/div/div[2]/span[2]/text()').get()
 
-    ## 产品review_tab请求链接
-    ## https://www.coupang.com/vp/product/reviews?productId=344529480&page=1&size=5&sortBy=ORDER_SCORE_ASC&ratings=&q=&viRoleCode=3&ratingSummary=true
+        ## 产品主要卖点prod-description
+        # prod-description-attribute
+        # prod_attr_item = selector_detailpage.xpath('//*[@class="prod-description"]/ul/li/text()').get()
+        # print(prod_attr_item)
+        # print(prod_buy_header_title, reviews_count,discount_rate,origin_price,prod_sale_price,unit_price,)
 
-    ## 销售公司信息
-    ## 与产品属性的tab在同一个json里
-    ## https://www.coupang.com/vp/products/5735830650/items/9639246678/vendoritems/76923492094
-    ## 从产品详情页地址可以得到productId, itemId, vendoritems.
-    url_detailpage_itemBrief = f'https://www.coupang.com/vp/products/{productId}/items/{itemId}/vendoritems/{vendoritems}'
-    response_detailpage_itemBrief = requests.get(url=url_detailpage_itemBrief, headers=headers)
-    response_itemBrief = response_detailpage_itemBrief.text
-    detailpage_itemBrief_json_data = json.loads(response_itemBrief)
-    # pprint.pprint(detailpage_itemBrief_json_data)
+        ## 产品review_tab请求链接
+        ## https://www.coupang.com/vp/product/reviews?productId=344529480&page=1&size=5&sortBy=ORDER_SCORE_ASC&ratings=&q=&viRoleCode=3&ratingSummary=true
 
-    # 용량 중량
-    bi_1 = detailpage_itemBrief_json_data['essentials'][0]['description']
-    # 제품 주요 사양
-    if "제품 주요 사양" not in response_detailpage_itemBrief:
-        bi_2 = ""
-    else:
-        bi_2 = detailpage_itemBrief_json_data['essentials'][1]['description']
-    # 사용기한 또는 개봉 후 사용기간
-    if "사용기한" not in response_detailpage_itemBrief:
-        bi_3 = ""
-    else:
-        bi_3 = detailpage_itemBrief_json_data['essentials'][2]['description']
-    # 사용방법
-    if "사용방법" not in response_detailpage_itemBrief:
-        bi_4 = ""
-    else:
-        bi_4 = detailpage_itemBrief_json_data['essentials'][3]['description']
-    # 화장품제조업자 및 화장품책임판매업자
-    if "화장품제조업자" not in response_detailpage_itemBrief:
-        bi_5 = ""
-        bi_5_1 = bi_5_2 = bi_5
-    else:
-        bi_5 = detailpage_itemBrief_json_data['essentials'][4]['description']
-        if "/" in bi_5:
-            # 通过判断区分화장품제조업자和화장품책임판매업자
-            # 제조사
-            bi_5_1_raw = re.findall(r"(.*)/", bi_5, re.S)[0]
-            # if ":" in bi_5_1_raw:
+        ## 销售公司信息
+        ## 与产品属性的tab在同一个json里
+        ## https://www.coupang.com/vp/products/5735830650/items/9639246678/vendoritems/76923492094
+        ## 从产品详情页地址可以得到productId, itemId, vendoritems.
+        url_detailpage_itemBrief = f'https://www.coupang.com/vp/products/{productId}/items/{itemId}/vendoritems/{vendoritems}'
+
+        response_detailpage_itemBrief = requests.get(url=url_detailpage_itemBrief, headers=headers)
+        response_itemBrief = response_detailpage_itemBrief.text
+        detailpage_itemBrief_json_data = json.loads(response_itemBrief)
+        pprint.pprint(detailpage_itemBrief_json_data)
+
+        # 용량 중량
+        bi_1 = detailpage_itemBrief_json_data['essentials'][0]['description']
+        # 제품 주요 사양
+        if "제품 주요 사양" not in response_itemBrief:
+            bi_2 = ""
+        else:
+            bi_2 = detailpage_itemBrief_json_data['essentials'][1]['description']
+        # 사용기한 또는 개봉 후 사용기간
+        if "사용기한" not in response_itemBrief:
+            bi_3 = ""
+        else:
+            bi_3 = detailpage_itemBrief_json_data['essentials'][2]['description']
+        # 사용방법
+        if "사용방법" not in response_itemBrief:
+            bi_4 = ""
+        else:
+            bi_4 = detailpage_itemBrief_json_data['essentials'][3]['description']
+        # 화장품제조업자 및 화장품책임판매업자
+        if "화장품제조업자" not in response_itemBrief:
+            bi_5 = ""
+            bi_5_1 = bi_5_2 = bi_5
+        else:
+            bi_5 = detailpage_itemBrief_json_data['essentials'][4]['description']
+            if "/" in bi_5:
+                # 通过判断区分화장품제조업자和화장품책임판매업자
+                # 제조사
+                bi_5_1_raw = re.findall(r"(.*)/", bi_5, re.S)[0]
+                # if ":" in bi_5_1_raw:
+                #     bi_5_1_raw_raw = re.findall(r"제조.*:(.*) ", bi_5_1_raw, re.S)[0]
+                #     bi_5_1 = "".join(bi_5_1_raw_raw).strip()
+                # else:
+                bi_5_1 = "".join(bi_5_1_raw).strip()
+                # 판매업자
+                bi_5_2_raw = re.findall(r"/(.*)", bi_5, re.S)[0]
+                # if ":" in bi_5_2_raw:
+                #     bi_5_2_raw_raw = re.findall(r"판매.*::(.*) ", bi_5_2_raw, re.S)[0]
+                #     bi_5_2 = "".join(bi_5_2_raw_raw).strip()
+                # else:
+                bi_5_2 = "".join(bi_5_2_raw).strip()
+            # elif ":" in bi_5:
             #     bi_5_1_raw_raw = re.findall(r"제조.*:(.*) ", bi_5_1_raw, re.S)[0]
             #     bi_5_1 = "".join(bi_5_1_raw_raw).strip()
-            # else:
-            bi_5_1 = "".join(bi_5_1_raw).strip()
-            # 판매업자
-            bi_5_2_raw = re.findall(r"/(.*)", bi_5, re.S)[0]
-            # if ":" in bi_5_2_raw:
             #     bi_5_2_raw_raw = re.findall(r"판매.*::(.*) ", bi_5_2_raw, re.S)[0]
             #     bi_5_2 = "".join(bi_5_2_raw_raw).strip()
-            # else:
-            bi_5_2 = "".join(bi_5_2_raw).strip()
-        # elif ":" in bi_5:
-        #     bi_5_1_raw_raw = re.findall(r"제조.*:(.*) ", bi_5_1_raw, re.S)[0]
-        #     bi_5_1 = "".join(bi_5_1_raw_raw).strip()
-        #     bi_5_2_raw_raw = re.findall(r"판매.*::(.*) ", bi_5_2_raw, re.S)[0]
-        #     bi_5_2 = "".join(bi_5_2_raw_raw).strip()
+            else:
+                bi_5_1 = bi_5_2 = bi_5
+
+        # 제조국
+        if "제조국" not in response_itemBrief:
+            bi_6 = ""
         else:
-            bi_5_1 = bi_5_2 = bi_5
+            bi_6 = re.findall('(.*?)제조국', response_itemBrief)
+            # bi_6 = detailpage_itemBrief_json_data['essentials']['description']
+            print('bi6', bi_6)
+            print(len(bi_6))
 
-    # 제조국
-    if "제조국" not in response_detailpage_itemBrief:
+        # 성분
+        if "표시하여야 하는 모든 성분" not in response_itemBrief:
+            bi_7 = ""
+        else:
+            bi_7 = detailpage_itemBrief_json_data['essentials'][6]['description']
+
+        # 기능성 화장품
+        if "기능성 화장품" not in response_itemBrief:
+            bi_8 = ""
+        else:
+            bi_8 = detailpage_itemBrief_json_data['essentials'][7]['description']
+
+        # 주의사항
+        if "주의사항" not in response_itemBrief:
+            bi_9 = ""
+        else:
+            bi_9 = detailpage_itemBrief_json_data['essentials'][8]['description']
+
+        # 품질보증기준
+        if "품질보증기준" not in response_itemBrief:
+            bi_10 = ""
+        else:
+            bi_10 = detailpage_itemBrief_json_data['essentials'][9]['description']
+
+        # 소비자상담관련 전화번호
+        if "소비자상담관련 전화번호" not in response_itemBrief:
+            bi_11 = ""
+        else:
+            bi_11 = detailpage_itemBrief_json_data['essentials'][10]['description']
+        # print(bi_1, bi_2,bi_3,bi_4,bi_5,bi_6,bi_7,bi_8,bi_9, bi_10, bi_11)
+
+        ## returnPolicyVo 退换政策销售者公司信息
+        # 사업자번호
+        bizNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['bizNum']
+        # 判断coupang直营和第三方专营
+        if bizNum == 0:
+            bizNum = ""
+            ecommReportNum = ""
+            # 사업장 소재지
+            repAddress = ""
+            # e-mail
+            repEmail = ""
+            # 联系人姓名
+            repPersonName = ""
+            # 연락처
+            repPhoneNum = ""
+            # 상호/대표자
+            sellerWithRepPersonName = "쿠팡(주) / 강한승,박대준"
+            # 商号
+            vendorName = "쿠팡"
+        else:
+            # 통신판매업 신고번호
+            ecommReportNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['ecommReportNum']
+            # 사업장 소재지
+            repAddress = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repAddress']
+            # e-mail
+            repEmail = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repEmail']
+            # 联系人姓名
+            repPersonName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repPersonName']
+            # 연락처
+            repPhoneNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repPhoneNum']
+            # 상호/대표자
+            sellerWithRepPersonName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['sellerWithRepPersonName']
+            # 商号
+            vendorName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['vendorName']
+        # print('流通商信息', bizNum,ecommReportNum,repAddress,repEmail, repPersonName,repPhoneNum, sellerWithRepPersonName, vendorName)
+    else:
+        prod_buy_header_title = "상품을 찾을 수 없습니다"
+        dp_img = ""
+        prod_sale_price = ""
+        discount_rate = ""
+        origin_price = ""
+        unit_price = ""
+        reviews_count = ""
+        brand_name = ""
+        brand_url = ""
+        bi_1 = ""
+        bi_2 = ""
+        bi_3 = ""
+        bi_4 = ""
+        bi_5 = ""
         bi_6 = ""
-    else:
-        bi_6 = detailpage_itemBrief_json_data['essentials'][5]['description']
-
-    # 성분
-    if "표시하여야 하는 모든 성분" not in response_detailpage_itemBrief:
         bi_7 = ""
-    else:
-        bi_7 = detailpage_itemBrief_json_data['essentials'][6]['description']
-
-    # 기능성 화장품
-    if "기능성 화장품" not in response_detailpage_itemBrief:
         bi_8 = ""
-    else:
-        bi_8 = detailpage_itemBrief_json_data['essentials'][7]['description']
-
-    # 주의사항
-    if "주의사항" not in response_detailpage_itemBrief:
         bi_9 = ""
-    else:
-        bi_9 = detailpage_itemBrief_json_data['essentials'][8]['description']
-
-    # 품질보증기준
-    if "품질보증기준" not in response_detailpage_itemBrief:
         bi_10 = ""
-    else:
-        bi_10 = detailpage_itemBrief_json_data['essentials'][9]['description']
-
-    # 소비자상담관련 전화번호
-    if "소비자상담관련 전화번호" not in response_detailpage_itemBrief:
         bi_11 = ""
-    else:
-        bi_11 = detailpage_itemBrief_json_data['essentials'][10]['description']
-    # print(bi_1, bi_2,bi_3,bi_4,bi_5,bi_6,bi_7,bi_8,bi_9, bi_10, bi_11)
-
-    ## returnPolicyVo 退换政策销售者公司信息
-    # 사업자번호
-    bizNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['bizNum']
-    # 判断coupang直营和第三方专营
-    if bizNum == 0:
+        vendorName = ""
         bizNum = ""
         ecommReportNum = ""
-        # 사업장 소재지
         repAddress = ""
-        # e-mail
         repEmail = ""
-        # 联系人姓名
         repPersonName = ""
-        # 연락처
         repPhoneNum = ""
-        # 상호/대표자
-        sellerWithRepPersonName = "쿠팡(주) / 강한승,박대준"
-        # 商号
-        vendorName = "쿠팡"
-    else:
-        # 통신판매업 신고번호
-        ecommReportNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['ecommReportNum']
-        # 사업장 소재지
-        repAddress = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repAddress']
-        # e-mail
-        repEmail = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repEmail']
-        # 联系人姓名
-        repPersonName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repPersonName']
-        # 연락처
-        repPhoneNum = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['repPhoneNum']
-        # 상호/대표자
-        sellerWithRepPersonName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['sellerWithRepPersonName']
-        # 商号
-        vendorName = detailpage_itemBrief_json_data['returnPolicyVo']['sellerDetailInfo']['vendorName']
-    # print('流通商信息', bizNum,ecommReportNum,repAddress,repEmail, repPersonName,repPhoneNum, sellerWithRepPersonName, vendorName)
+        sellerWithRepPersonName = ""
+
 
     #时间戳
     rightnow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
