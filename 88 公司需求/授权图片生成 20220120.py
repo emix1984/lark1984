b445dbcfedb6d881a8feb97f75dc056c83e2feec
lark1984 <2909755020@qq.com>
2022-01-28 01:03:21 +0900

    公司需求授权图片生成控制

--- 88 公司需求/授权图片生成 20220120.py
+++ 88 公司需求/授权图片生成 20220120.py
@@ -3,18 +3,25 @@
 # https://blog.csdn.net/python_limiao/article/details/83549280
 
 from PIL import Image, ImageDraw, ImageFont
-img = Image.open("b.jpg")
+img = Image.open("db1.jpg")
 draw = ImageDraw.Draw(img) # 生成绘制对象draw
-typeface = ImageFont.truetype('simkai.ttf', 38)
+typeface1 = ImageFont.truetype('simkai.ttf', 80)
+typeface2 = ImageFont.truetype('simkai.ttf', 53)
 shopname = "店铺名称"
 shopid = "2909755020"
 shopweb = "www.tmall.com"
+channel = "天猫国际渠道"
 
-text = f"{shopname}   \n{shopid}   \n{shopweb}"
+datefrom = "2021   01   01      2022+++12+++12"
+today = "2022   01   13"
+code = "a20222022022013"
 
-
+text1 = f"{shopname}   \n{shopid}   \n{shopweb}   \n{channel}"
+text2 = f"{datefrom}   \n{today}   \n{code}"
 # darw.text()回执文字并生成图片
-draw.text((60, 150), text, fill=(120, 0, 60),
-font=typeface)
+draw.text((800, 850), text1, fill=(120, 0, 60),
+font=typeface1)
+draw.text((490, 1600), text2, fill=(120, 0, 60),
+font=typeface2)
 # img.show()
-img.save("斗图小组.png") # 保存
+img.save("斗图小组.jpg") # 保存
